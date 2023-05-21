ebb3fd9d7e6fe2abe3b4f9e66291d1588661786d
Andranik Sargsyan <and.sargsyan@yahoo.com>
2023-05-21 12:14:44 +0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
+import os
 from setuptools import setup
+
+package_dir = os.path.dirname(os.path.join(__file__))
+
+requires = []
+with open(os.path.join(package_dir, 'requirements.txt')) as f:
+    for item in f.read().splitlines():
+        if not item.strip().lower().startswith('--'):
+            requires.append(item)
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
@@ -13,4 +22,5 @@ setup(
     keywords='aerial-image-to-map',
     url='https://github.com/AndranikSargsyan/qartezator',
     packages=['qartezator'],
+    install_requires=requires
 )
