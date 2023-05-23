713a1ac67808f38136881c8fb1549178a5d75fc1
astghikchobanyan <astghik.chobanyan@cognaize.com>
2023-05-24 00:24:28 +0400

    add generator and discriminator models

--- qartezator/models/discriminators.py
+++ qartezator/models/discriminators.py
@@ -1,57 +1,50 @@
-# original: https://github.com/NVIDIA/pix2pixHD/blob/master/models/networks.py
-from collections import defaultdict
-
-import numpy as np
+import torch
 import torch.nn as nn
 
 
-# Defines the PatchGAN discriminator with the specified arguments.
-class NLayerDiscriminator(nn.Module):
-    def __init__(self, input_nc, ndf=64, n_layers=3, norm_layer=nn.BatchNorm2d,):
+class CNNBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=2):
         super().__init__()
-        self.n_layers = n_layers
-
-        kw = 4
-        padw = int(np.ceil((kw-1.0)/2))
-        sequence = [[nn.Conv2d(input_nc, ndf, kernel_size=kw, stride=2, padding=padw),
-                     nn.LeakyReLU(0.2, True)]]
-
-        nf = ndf
-        for n in range(1, n_layers):
-            nf_prev = nf
-            nf = min(nf * 2, 512)
-
-            cur_model = []
-            cur_model += [
-                nn.Conv2d(nf_prev, nf, kernel_size=kw, stride=2, padding=padw),
-                norm_layer(nf),
-                nn.LeakyReLU(0.2, True)
-            ]
-            sequence.append(cur_model)
-
-        nf_prev = nf
-        nf = min(nf * 2, 512)
-
-        cur_model = []
-        cur_model += [
-            nn.Conv2d(nf_prev, nf, kernel_size=kw, stride=1, padding=padw),
-            norm_layer(nf),
-            nn.LeakyReLU(0.2, True)
-        ]
-        sequence.append(cur_model)
-
-        sequence += [[nn.Conv2d(nf, 1, kernel_size=kw, stride=1, padding=padw)]]
-
-        for n in range(len(sequence)):
-            setattr(self, 'model'+str(n), nn.Sequential(*sequence[n]))
-
-    def get_all_activations(self, x):
-        res = [x]
-        for n in range(self.n_layers + 2):
-            model = getattr(self, 'model' + str(n))
-            res.append(model(res[-1]))
-        return res[1:]
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
+                      kernel_size=4, stride=stride, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(0.2)
+        )
 
     def forward(self, x):
-        act = self.get_all_activations(x)
-        return act[-1], act[:-1]
+        return self.conv(x)
+
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels=3, features=[64, 128, 256, 512]):
+        super().__init__()
+        self.initial = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels*2, out_channels=features[0],
+                      kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2)
+        )
+
+        layers = []
+        in_channels = features[0]
+        for feature in features[1:]:
+            layers.append(
+                CNNBlock(in_channels=in_channels, out_channels=feature,
+                         stride=1 if feature == features[-1] else 2)
+
+            )
+            in_channels = feature
+        layers.append(
+            nn.Conv2d(
+                in_channels=in_channels, out_channels=1, kernel_size=4,
+                stride=1, padding=1
+            )
+        )
+
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x, y):
+
+        x = torch.cat([x, y], dim=1)
+        x = self.initial(x)
+        return self.model(x)
